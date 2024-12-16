48fed83fc816c4446bf1f244da1a058d7785b8cf
Philippe <louis@louis>
2024-12-17 06:44:28 +0800

    added some quotes for noel and me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ function About() {
               Our mission is to provide innovative solutions 
               to complex problems.  <br />We strive to be the best in our field.
             </p>
-            <Button variant="outline">Donate</Button>
+          
         </div>
         <img src="book.gif" alt="" className='pics-1' style={{ width: "400px", marginRight:"10px" }}/>
       </div>
@@ -29,8 +29,8 @@ function About() {
         <div>
           <h2 className="scroll-m-20  pb-2 text-3xl font-semibold tracking-tight first:mt-0">Meet our <br /> Creative Team</h2>
           <p className="leading-7 [&:not(:first-child)]:mt-6">
-            A group of colleges  who are passionate about <br />
-            creating innovative solutions
+            A group of college students who are passionate about <br />
+            creating innovative solutions.
           </p>
 
         </div>
@@ -76,7 +76,7 @@ function About() {
             Noel Salazar
           </h4>
           <p className="text-sm text-muted-foreground">Fullstack Developer</p>
-          <blockquote className="mt-4 border-l-2 pl-2 italic text-sm mb-8">Lorem ipsum creating innovative solutions creating innovative solutions</blockquote>
+          <blockquote className="mt-4 border-l-2 pl-2 italic text-sm mb-8">"a hundred knots in the intestines of sorrow, seeking aspiration through ink and paper."</blockquote>
           <div className='socials'>
           <Tooltip showArrow={true} placement='bottom' content="Noel Salazar">
             <a href="https://www.facebook.com/profile.php?id=100063708997272">
@@ -141,7 +141,7 @@ function About() {
           <img src="prof2.jpg" alt="" className='profile'/>
           <h4 className="scroll-m-20 text-base font-semibold tracking-tight mt-2" >Philippe Louis Garibay</h4>
           <p className="text-sm text-muted-foreground">Frontend Developer</p>
-          <blockquote className="mt-4 border-l-2 pl-2 italic text-sm mb-8">Lorem ipsum creating innovative solutions creating innovative solutions</blockquote>
+          <blockquote className="mt-4 border-l-2 pl-2 italic text-sm mb-8">"i think a soulmate is someone who will make you be the most "you" that you can possibly be"</blockquote>
           <div className='socials'>
           <Tooltip showArrow={true} placement='bottom' content="Philippe Garibay" >
               <a href="www.facebook.com">
